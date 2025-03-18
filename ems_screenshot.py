96e752a8583a70e5d01f2b1a517fb5f0da0dedd9
Juri Hubrig <juri.hubrig@fu-berlin.de>
2025-03-18 10:44:52 +0100

    Added more comments and removed substraction of time from sleep in last loop

--- ems_screenshot.py
+++ ems_screenshot.py
@@ -1,6 +1,9 @@
-__version__ = "1.2.1a"
+# Description: Takes screenshot(s) of the DWD or UWZ warning maps or metmaps.eu
+__version__ = "1.2.2"
 __author__  = "Juri Hubrig"
 
+
+# import necessary modules
 import sys
 import logging
 import argparse
@@ -13,15 +16,22 @@ from datetime import datetime as dt, timedelta as td
 
 
 def u(output_dir, user_agent, dt_utc, username, password, URL):
-   
+   """
+   Takes a screenshot of the UWZ weather warning map for Germany.
+   """
+   # set the URL and image path
    URL         = 'https://www.weatherpro.com/de/germany/berlin/berlin/iframe?mapregion=deutschland'
    image_path  = f'{output_dir}/uwz_{dt_minutes_file(dt_utc)}.png'
 
+   # use playwright to take a screenshot of the UWZ weather warning map
    with sync_playwright() as p:
+      # open the browser and go to the URL
       browser  = p.chromium.launch()
       context  = browser.new_context(user_agent=user_agent)
       #context.set_default_timeout(55000)
       page     = context.new_page()
+      # set the viewport size
+      #page.set_viewport_size({"width": 556, "height": 600})
       try:
          page.goto(URL)
          page.wait_for_load_state("load")
@@ -48,6 +58,7 @@ def u(output_dir, user_agent, dt_utc, username, password, URL):
          locator = page.locator('#mapContainer').locator('.leaflet-map-pane').locator('.leaflet-overlay-pane').locator('.leaflet-zoom-animated').locator('g')
          locator.wait_for()
          page.wait_for_load_state('networkidle')
+      # if an error occurs, handle it
       except Exception as e:
          if verbose:
             print(e)
@@ -59,6 +70,7 @@ def u(output_dir, user_agent, dt_utc, username, password, URL):
             logger.error(f"{dtime}\n{err}\n{trace}{'-'*114}")
          browser.close(); return
       
+      # take a screenshot of the UWZ weather warning map
       page.screenshot(
          path = image_path,
          clip = {
@@ -68,39 +80,33 @@ def u(output_dir, user_agent, dt_utc, username, password, URL):
             "height":   600
          }
       )
+      # close the browser
       browser.close()
    
+   # add watermark to the image
    if args.watermark: watermark(image_path, "bl", dt_utc)
    
 
 def d(output_dir, user_agent, dt_utc, username, password, URL):
-   
+   """
+   Takes a screenshot of the DWD weather warning map for Germany.
+   """
+   # set the URL and image path
    URL         = 'https://www.dwd.de/DE/wetter/warnungen_landkreise/warnWetter_node.html'
    image_path  = path = f'{output_dir}/dwd_{dt_minutes_file(dt_utc)}.png'
-
+   
+   # use playwright to take a screenshot of the DWD weather warning map
    with sync_playwright() as p:
       
+      # open the browser and go to the URL
       browser  = p.chromium.launch()
       context  = browser.new_context(user_agent=user_agent)
       page     = context.new_page()
       
+      # try to go to the URL
       try:
          page.goto(URL)
-      except Exception as e:
-         if verbose:
-            print(e)
-            traceback.print_exc()
-         if log:
-            dtime = utcnow_seconds().strftime("%Y-%m-%d %H:%M:%S")
-            err   = f"{e.__class__.__name__}: {e}"
-            trace = traceback.format_exc()
-            logger.error(f"{dtime}\n{err}\n{trace}{'-'*114}")
-         browser.close(); return
-
-      try:
-         page.wait_for_selector('#appBox') 
-         page.wait_for_selector('#headerBox')
-         page.wait_for_selector('#svgBox')
+      # if an error occurs, handle it
       except Exception as e:
          if verbose:
             print(e)
@@ -112,10 +118,29 @@ def d(output_dir, user_agent, dt_utc, username, password, URL):
             logger.error(f"{dtime}\n{err}\n{trace}{'-'*114}")
          browser.close(); return
       
+      # wait for the map to be fully loaded
+      try:
+         page.wait_for_selector('#appBox') 
+         page.wait_for_selector('#headerBox')
+         page.wait_for_selector('#svgBox')
+      # if an error occurs, handle it
+      except Exception as e:
+         if verbose:
+            print(e)
+            traceback.print_exc()
+         if log:
+            dtime = utcnow_seconds().strftime("%Y-%m-%d %H:%M:%S")
+            err   = f"{e.__class__.__name__}: {e}"
+            trace = traceback.format_exc()
+            logger.error(f"{dtime}\n{err}\n{trace}{'-'*114}")
+         browser.close(); return
+      
+      # get the bounding boxes of the elements
       app_box           = page.locator('#appBox').bounding_box()
       header_box        = page.locator('#headerBox').bounding_box()
       svg_box           = page.locator('#svgBox').bounding_box()
-
+      
+      # take a screenshot of the DWD weather warning map
       page.screenshot(
          path = image_path,
          clip = {       
@@ -125,27 +150,38 @@ def d(output_dir, user_agent, dt_utc, username, password, URL):
             "height":   svg_box['height'] + header_box['height']
          }
       )
+      # close the browser
       browser.close()
    
+   # add watermark to the image
    if args.watermark: watermark(image_path, "br", dt_utc)
 
 
 def m(output_dir, user_agent, dt_utc, username, password, URL):
-   
+   """
+   Takes a screenshot of the MetMaps page using the given URL.
+   """
+   # set the image path
    image_path = f'{output_dir}/metmaps_{dt_minutes_file(dt_utc)}.png'
-
-   with sync_playwright() as p:
+   
+   # use playwright to take a screenshot of the MetMaps page
+   with sync_playwright() as p:  
       
+      # open the browser and go to the URL
       browser  = p.chromium.launch()
       context  = browser.new_context(
+         # set the user agent to the given user agent
          user_agent        = user_agent,
          http_credentials  = {"username": username, "password": password}
       )
+      # create a new page
       page     = context.new_page()
       
+      # try to go to the URL
       try:
          page.goto(URL)
          page.wait_for_load_state('networkidle')
+      # if an error occurs, handle it
       except Exception as e:
          if verbose:
             print(e)
@@ -164,8 +200,10 @@ def m(output_dir, user_agent, dt_utc, username, password, URL):
          .locator('span#outline').locator('input#inputimage')
       inputimage.wait_for() 
       """
+      # try to get the bounding box of the image
       try:
          inputimage = page.locator('#inputimage').bounding_box()
+      # if an error occurs, handle it
       except Exception as e:
          if verbose:
             print(e)
@@ -177,6 +215,7 @@ def m(output_dir, user_agent, dt_utc, username, password, URL):
             logger.error(f"{dtime}\n{err}\n{trace}{'-'*114}")
          browser.close(); return
       
+      # take a screenshot of the image
       page.screenshot(
          path = image_path,
          clip = {       
@@ -186,8 +225,10 @@ def m(output_dir, user_agent, dt_utc, username, password, URL):
             "height":   inputimage['height']
          }
       )
+      # close the browser
       browser.close()
    
+   # add watermark to the image
    if args.watermark: watermark(image_path, "br", dt_utc)
 
 
@@ -197,30 +238,42 @@ from playwright.sync_api import sync_playwright
    
 
 def watermark(image_path, position, dt_utc):
-   
+   """ 
+   Adds a datetime watermark to the image.
+   """
    from PIL import Image, ImageFont, ImageDraw
    
+   # open the image
    image             = Image.open(image_path)
    watermark_image   = image.copy()
    draw              = ImageDraw.Draw(watermark_image)
    
+   # get the size of the image 
    w, h = image.size
+   # set the position of the watermark  
    if position == "bl":
+      # bottom left
       x, y = int(w / 4), int(h - h / 50)
+      # set the font size, depending on the height of the image
       font_size = int(h/18)
    elif position == "br":
+      # bottom right 
       x, y = int(w - w / 5), int(h - h / 50)
+      # set the font size, depending on the width of the image
       font_size = int(w/20)
    
    #font = ImageFont.truetype("arial.ttf", int(font_size/12))
    font = ImageFont.load_default(size=font_size)
    # add watermark
    draw.text((x, y), dt_minutes_mark(dt_utc), fill="red", font=font, anchor='ms')
+   # save the image with the watermark
    watermark_image.save(image_path)
 
 
+# datetime functions for easier handling
 utcnow_minutes    = lambda : dt.utcnow().replace(second=0, microsecond=0)
 utcnow_seconds    = lambda : dt.utcnow().replace(microsecond=0)
+
 dt_minutes        = lambda dt_utc : dt_utc.replace(second=0, microsecond=0)
 dt_minutes_file   = lambda dt_utc : dt_utc.strftime("%Y-%m-%d_%H:%M")
 dt_minutes_mark   = lambda dt_utc : dt_utc.strftime("%Y-%m-%d %H:%M")
@@ -228,12 +281,13 @@ dt_minutes_mark   = lambda dt_utc : dt_utc.strftime("%Y-%m-%d %H:%M")
 
 if __name__ == '__main__':
    
-   # read config file
+   # read the config file
    config = configparser.ConfigParser()
    config.read('config.ini')
    
    # facilitate access to general config
    cf_general        = config["general"]
+   # get general config settings
    cf_start_datetime = cf_general["start_datetime"]
    cf_end_datetime   = cf_general["end_datetime"]
    cf_output_dir     = cf_general["output_dir"]
@@ -246,6 +300,7 @@ if __name__ == '__main__':
 
    # same with metmaps-specific config
    cf_metmaps  = config["metmaps"]
+   # get metmaps-specific config settings
    cf_username = cf_metmaps["username"]
    cf_password = cf_metmaps["password"]
    cf_URL      = cf_metmaps["URL"]
@@ -253,11 +308,13 @@ if __name__ == '__main__':
 
    # now parse command line arguments which overwrite the default config settings
    parser = argparse.ArgumentParser(
+      # program name and description
       prog        = "EMS Screenshot Tool",
       description = "Takes a screenshot from the DWD or UWZ warning maps or metmaps.eu",
       epilog      = f"© 2025 {__author__}, Version {__version__}"
    )
    
+   # add command line arguments
    parser.add_argument('start_end', nargs="*", default=[cf_start_datetime, cf_end_datetime], help="Start/end datetimes or date (format: YYYYMMDDhhmm OR hhmm). Can also be 'now' to start on the next full minute or 'max' (only second argument). If both arguments are 'now' (or no argument given with default config): take screenshot(s) immediatly.")
    parser.add_argument('-s', '--sites', default=cf_sites, help="Choose sites to screenshot ([a]ll, [u]wz, [d]wd, [m]etmaps)")
    parser.add_argument('-i', '--interval', default=cf_interval, type=int, help="Recording interval in minutes")
@@ -271,10 +328,11 @@ if __name__ == '__main__':
    parser.add_argument('-v', '--verbose', action='store_true', default=cf_verbose, help="Print verbose output")
    
    
+   # parse command line arguments
    args     = parser.parse_args()
+   # set verbose and log variables for the whole script
    verbose  = args.verbose
    
-
    # if logging is turned off: log error messages
    if args.log:
       log         = True
@@ -289,10 +347,11 @@ if __name__ == '__main__':
    # configure the error log
    logging.basicConfig(filename='error.log', level=log_level)
    
-
+   # if all sites are desired, take all sites, else only the ones specified
    if args.sites == 'a':
       sites = all_sites[:]
    else:
+      # check if all sites are in the list of all sites
       sites = [ i for i in all_sites if i in args.sites ]
    
    # create output directory if not existing yet
@@ -300,7 +359,9 @@ if __name__ == '__main__':
 
    
    def get_screenshots():
-       
+      """
+      Takes screenshots of the desired sites.
+      """
       # take screenshots for each desired website
       for site in sites:
          site_function = globals()[site]
@@ -353,18 +414,21 @@ if __name__ == '__main__':
                # see https://stackoverflow.com/questions/11551996/why-do-we-need-the-finally-clause-in-python
                finally: continue
             finally: continue
-         finally: continue
+         finally: continue 
    
-   
+
    # if 2 command line arguments are present, take them as start and end datetimes
-   if len(args.start_end) == 2:
+   if len(args.start_end) == 2: 
       start_datetime, end_datetime = args.start_end
-   # if only 1 command line argument is given, assume it is meant for datetime_start, take end_datetime from config
+   # if only 1 or no command line arguments are present, take the start and end datetimes from the config
    elif len(args.start_end) == 1:
+      # if only 1 command line argument is given, take it as start_datetime
+      # and set end_datetime to the default end_datetime from the config
       start_datetime, end_datetime = args.start_end[0], cfg.end_datetime
    else: sys.exit("Please enter 1, 2 or NO command line argument(s)")
    
-
+   
+   # check if start_datetime is 'now' or a valid datetime
    if start_datetime == "now":
       # get difference from now to the next full minute, so we know how much time we have to run the script
       dt_now         = dt.utcnow()
@@ -375,59 +439,86 @@ if __name__ == '__main__':
       # this is necessary so we can make screenshots every minute, as early as possible
       # it should be a very rare case, but better save than sorry, so we try to cover it
       # even if the scripts runs very slowly, 1 second from here to the while loop should be enough
+      # if not, we add 1 second to the buffer time
       if (dt_next_minute - dt_now).total_seconds() < 1:
          buffer_time += 1
       
-      # add the extra minute + buffer time
+      # add the extra minute + buffer time to the current time
       start_datetime = utcnow_minutes() + td(minutes = 1+buffer_time )
    
    elif len(start_datetime) == 4:
+      # if only HHMM is given, take the current date and time
       dt_now         = dt.utcnow()
       HHMM           = start_datetime
       start_datetime = dt(dt_now.year, dt_now.month, dt_now.day, int(HHMM[0:2]), int(HHMM[2:]))
    elif len(start_datetime) == 12:
+      # if YYYYmmddHHMM is given, take the given date and time
       YYYY           = start_datetime[0:4]
       mmdd           = start_datetime[4:8]
       HHMM           = start_datetime[8:]
       start_datetime = dt(int(YYYY), int(mmdd[0:2]), int(mmdd[2:]), int(HHMM[0:2]), int(HHMM[2:]))
+   # if the input is not valid, exit the script
    else: sys.exit("WRONG INPUT: start_datetime has to be 4 or 12 characters long!")
    
+   # if the start_datetime is in the past, exit the script
    if start_datetime < dt.utcnow():
       sys.exit("WRONG INPUT: start_datetime needs to be NOW or in the future!")
    
-   
+
+   # check if end_datetime is 'max' or a valid datetime
    if end_datetime == "max":
+      # if end_datetime is 'max', set it to the maximum possible datetime
       from datetime import MAXYEAR
       end_datetime = dt(MAXYEAR, 12, 31, 23, 59)
+   # if end_datetime is 'now', take a screenshot immediately and exit the script
    elif len(end_datetime) == 4:
+      # if only HHMM is given, take the current date and time
       dt_now         = dt.utcnow()
       HHMM           = end_datetime
       end_datetime   = dt(dt_now.year, dt_now.month, dt_now.day, int(HHMM[0:2]), int(HHMM[2:]))
    elif len(end_datetime) == 12:
+      # if YYYYmmddHHMM is given, take the given date and time
       YYYY           = end_datetime[0:4]
       mmdd           = end_datetime[4:8]
       HHMM           = end_datetime[8:]
       end_datetime   = dt(int(YYYY), int(mmdd[0:2]), int(mmdd[2:]), int(HHMM[0:2]), int(HHMM[2:]))
    elif end_datetime == "now":
-      # take screenshot immediately and exit script afterwards
+      # if end_datetime is 'now', take a screenshot immediately and exit the script
       if verbose: print("Taking screenshot(s) NOW...")
       get_screenshots()
       sys.exit()
+   # if the input is not valid, exit the script
    else: sys.exit("WRONG INPUT: end_datetime has to be 4 or 12 characters long!")
    
+   # if the end_datetime is in the past, exit the script
    if end_datetime <= dt.utcnow():
       sys.exit("WRONG INPUT: end_datetime needs to be in the future!")
    
 
    # wait for start time to begin
    while dt.utcnow() < start_datetime:
+      # if verbose is True, print the remaining time
+      if verbose:
+         # get the remaining time until the start_datetime
+         remaining_time = start_datetime - dt.utcnow()
+         # clear the all previous output and print the remaining time
+         print("\033c", end="")
+         print(f"Waiting for {remaining_time} to start...")
+      # sleep for a very short time to not overload the CPU
       sleep(0.0000001)
    
+   # if verbose is True, print that we are starting to take screenshots now
    if verbose: print("Starting to take screenshot(s) NOW...")
-
+   
+   # start taking screenshots
    while dt.utcnow() <= end_datetime:
-      before = dt.utcnow()
+      
+      # if verbose is True, print the current datetime
+      if verbose:
+         # clear the all previous output and print the current datetime
+         print("\033c", end="")
+         print(f"Taking screenshot(s) at {dt.utcnow()}...")
+      # take screenshots of all desired sites
       get_screenshots()
-      after  = dt.utcnow()
-      # we want to sleep for the interval minus how long get_screeshots() fuction execution took
-      sleep( args.interval - (after-before).total_seconds()/60 )
+      # sleep for the given interval
+      sleep(args.interval)
